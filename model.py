3fbc9eb7c329683c8aa4a482b5080b16645ff6ef
langstonleeWM <llee02@email.wm.edu>
2023-04-16 20:12:38 -0400

    code cleanup and refactor

--- model.py
+++ model.py
@@ -1,37 +1,12 @@
-"""
-define moduals of model
-"""
 from torch.autograd import Variable
 import torch.nn.functional as F
 import torch.nn as nn
 
-## MLPModel()
 class CNNModel(nn.Module):
-	"""docstring for ClassName"""
 	
 	def __init__(self):
 		super(CNNModel, self).__init__()
-		##-----------------------------------------------------------
-		## define the model architecture here
-		## MNIST image input size batch * 28 * 28 (one input channel)
-		##-----------------------------------------------------------
 		
-		## define CNN layers below
-		# self.conv1 = nn.Sequential( nn.Conv2d(1, 32, kernel_size=3),
-		# 							nn.ReLU(),
-		# 							nn.MaxPool2d(kernel_size=2),
-		# 							nn.Dropout(0.2),
-		# 						)
-		# self.conv2 = nn.Sequential( nn.Conv2d(32, 32, kernel_size=3),
-		# 							nn.ReLU(),
-		# 							nn.MaxPool2d(kernel_size=2),
-		# 							nn.Dropout(0.2),
-		# 						)
-		# self.conv3 = nn.Sequential( nn.Conv2d(32, 64, kernel_size=3),
-		# 							nn.ReLU(),
-		# 							nn.MaxPool2d(kernel_size=2),
-		# 							nn.Dropout(0.2),
-		# 						)
 		self.conv_layers = nn.Sequential( 	
 			#1st conv layer
 			nn.Conv2d(in_channels=1, out_channels=10, kernel_size=3, stride=1, padding=1),
@@ -49,15 +24,6 @@ class CNNModel(nn.Module):
 			nn.BatchNorm2d(30),
 			nn.Dropout(0.2),
 		)
-		##------------------------------------------------
-		## write code to define fully connected layer below
-		##------------------------------------------------
-		# self.fc1 = nn.Sequential( 	nn.Linear(3*3*64, 256),
-		# 							nn.ReLU(),
-		# 							nn.MaxPool2d(kernel_size=2),
-		# 							nn.Dropout(0.2),
-		# 						)
-		# self.fc2 = nn.Linear(256, 10)
 		
 		self.fc_layers = nn.Sequential(
 			nn.Linear(7*7*30, 256),
@@ -66,28 +32,8 @@ class CNNModel(nn.Module):
 			nn.Linear(256, 10)
 		)
 
-	'''feed features to the model'''
-	def forward(self, x):  #default
-		
-		##---------------------------------------------------------
-		## write code to feed input features to the CNN models defined above
-		##---------------------------------------------------------
+	def forward(self, x):
 		x = self.conv_layers(x)
-		print(x.shape)
-		## write flatten tensor code below (it is done)
-		#x = torch.flatten(x_out,1) # x_out is output of last layer
 		x = x.view(-1, 7*7*30)  
 		x = self.fc_layers(x)
-
-
-		## ---------------------------------------------------
-		## write fully connected layer (Linear layer) below
-		## ---------------------------------------------------
-		
-		
 		return x
-        
-		
-		
-	
-		
--- utils.py
+++ utils.py
@@ -1,18 +1,35 @@
-"""utils.py"""
+import torchvision.transforms as transforms
+import torchvision
+from torch.utils.data import DataLoader
 
-import argparse
-import subprocess
+def _load_data(DATA_PATH, batch_size, rotation):
+	'''Data loader'''
 
-'''
-fun: convert string to boolean: True and False
-'''
-def str2bool(v):
-    # codes from : https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+	print("data_path: ", DATA_PATH)
+	train_trans = transforms.Compose([transforms.RandomRotation(rotation),transforms.RandomHorizontalFlip(),\
+								transforms.ToTensor(), transforms.Normalize((0.5), (0.5))])
+	
+	train_dataset = torchvision.datasets.MNIST(root=DATA_PATH, download=True,train=True, transform=train_trans)
+	train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True,num_workers=8)
 
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-    
+	test_trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5), (0.5))])
+	test_dataset = torchvision.datasets.MNIST(root=DATA_PATH, download=True, train=False, transform=test_trans)
+	test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False, num_workers=8)
+	
+	return train_loader, test_loader
+
+def _compute_accuracy(y_pred, y_batch):
+	return (y_pred==y_batch).sum().item()
+
+def adjust_learning_rate(learning_rate, optimizer, epoch, decay):
+	"""Sets the learning rate to the initial LR decayed by 1/10 every args.lr epochs"""
+	lr = learning_rate
+	if (epoch > 5):
+		lr = 0.001
+	if (epoch >= 10):
+		lr = 0.0001
+	if (epoch > 20):
+		lr = 0.00001
+	
+	for param_group in optimizer.param_groups:
+		param_group['lr'] = lr
